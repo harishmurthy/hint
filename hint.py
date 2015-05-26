c47fb2f01009fb43eb21be0bb47449ad49c2d6b1
Harish Murthy <harish.v.murthy@gmail.com>
2015-05-26 09:39:05 +0530

    Collision free version

--- hint.py
+++ hint.py
@@ -5,16 +5,18 @@ import random
 _table = ["%c" % x for x in range(ord('A'),ord('Z')+1)]
 _table = _table + ["%c" % x for x in range(ord('a'),ord('z')+1)]
 _table = _table + ["%c" % x for x in range(ord('0'),ord('9')+1)]
+_table.append('#')
+_table.append('$')
 
-_pows = [len(_table)**x for x in range(1,10)]
+_pows = [(len(_table)**x)-1 for x in range(1,10)]
 
 _numdigits = 1
 
 _counter = 0
 
 _mem = {}
-
-_collision = 0
+_mem[0] = _table
+random.shuffle(_mem[0])
 
 def gethint():
   global _table
@@ -22,21 +24,18 @@ def gethint():
   global _numdigits
   global _counter
   global _mem
-  global _collision
   h = ''
+  curr = _counter
   for x in range(_numdigits):
-    h = h + random.choice(_table)
-  while h in _mem:
-    h = ''
-    print('collision ' + str(_collision))
-    _collision = _collision + 1
-    for x in range(_numdigits):
-      h = h + random.choice(_table)
+    digit = curr & 0x3f
+    curr = curr >> 6
+    h = h + _mem[x][digit]
   _counter = _counter + 1
   if _counter in _pows:
+    _mem[_numdigits] = _table
+    random.shuffle(_mem[_numdigits])
     _numdigits = _numdigits + 1
-  _mem[h] = _counter
-  return h
+  return h[::-1]
 
 if __name__ == "__main__":
   for x in range(1000):
